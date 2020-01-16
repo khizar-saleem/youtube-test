a6f0347b497e070b4c0535efe8fe8795b3d4cbb0
Khizar Saleem <saleemkhizar@outlook.com>
2020-01-15 17:00:08 -0700

    added ability to play a video using video key in portrait mode

--- app/src/main/java/com/khizarms/youtubetest/MainActivity.java
+++ app/src/main/java/com/khizarms/youtubetest/MainActivity.java
@@ -1,14 +1,45 @@
 package com.khizarms.youtubetest;
 
+import android.view.View;
+import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import com.google.android.youtube.player.YouTubePlayer.Provider;
+import com.google.android.youtube.player.YouTubePlayerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends YouTubeBaseActivity {
+
+  YouTubePlayerView youTubePlayerView;
+  OnInitializedListener onInitializedListener;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    youTubePlayerView = findViewById(R.id.yt_player_view);
+    onInitializedListener = new OnInitializedListener() {
+      @Override
+      public void onInitializationSuccess(Provider provider, YouTubePlayer youTubePlayer,
+          boolean b) {
+        youTubePlayer.loadVideo("b0U8EyHR4_Y");
+      }
+
+      @Override
+      public void onInitializationFailure(Provider provider,
+          YouTubeInitializationResult youTubeInitializationResult) {
+
+      }
+    };
+    youTubePlayerView.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        youTubePlayerView.initialize(PlayerConfig.API_KEY, onInitializedListener);
+      }
+    });
   }
 
 }
---
+++ app/src/main/java/com/khizarms/youtubetest/PlayerConfig.java
@@ -0,0 +1,9 @@
+package com.khizarms.youtubetest;
+
+public class PlayerConfig {
+
+  public PlayerConfig() {
+  }
+
+  public static final String API_KEY = "AIzaSyCKKHjlftElZQ-ATdbYgwuHq1npC8isaMI";
+}
