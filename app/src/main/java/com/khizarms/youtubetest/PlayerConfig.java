53914fb5f3ddb7396480ae51681ff8cc2670c025
Khizar Saleem <saleemkhizar@outlook.com>
2020-01-15 17:15:12 -0700

    Delete PlayerConfig.java

--- app/src/main/java/com/khizarms/youtubetest/PlayerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.khizarms.youtubetest;
-
-public class PlayerConfig {
-
-  public PlayerConfig() {
-  }
-
-  public static final String API_KEY = "AIzaSyCKKHjlftElZQ-ATdbYgwuHq1npC8isaMI";
-}
